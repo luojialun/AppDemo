ca3c668b50f88ee8860cc89ae17be8aad3a9b059
luojialun <623572967@qq.com>
2019-07-14 14:27:27 +0800

    仿微博开屏style

---
+++ app/src/main/java/com/android/mydemoapp/recyclerview/RecyclerviewActivity.java
@@ -0,0 +1,22 @@
+package com.android.mydemoapp.recyclerview;
+
+import com.android.mydemoapp.R;
+import com.android.mydemoapp.base.BaseActivity;
+
+public class RecyclerviewActivity extends BaseActivity {
+
+    @Override
+    public int setViewId() {
+        return R.layout.activity_recyclerview;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/android/mydemoapp/splash/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.android.mydemoapp.splash;
+
+import com.android.mydemoapp.R;
+import com.android.mydemoapp.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    public int setViewId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
